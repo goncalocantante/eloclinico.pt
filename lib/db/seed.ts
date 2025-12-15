1e66b16e5a83b4222e8e9ab12019160f468de680
Gonçalo Cantante <goncalocantante@gmail.com>
2025-12-15 15:12:41 +0000

    fix build errors

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -1,29 +1,4 @@
-import { db } from "./drizzle";
-import { users, patients, Patient } from "./schema";
-import { faker } from "@faker-js/faker";
-import { unique } from "@dpaskhin/unique";
-
-async function createInitialPatients() {
-  // Create 100 clients using realistic mock data from faker
-  const patientsMock: Patient[] = Array.from({ length: 100 }, () => ({
-    id: faker.string.uuid(),
-    name: faker.person.fullName(),
-    email: unique(faker.internet.email()),
-    phone: faker.phone.number({ style: "international" }),
-    position: faker.person.jobTitle(),
-    address: faker.location.streetAddress(),
-    //  profileImage: faker.image.avatar(),
-    information: faker.lorem.paragraphs(),
-    dateOfBirth: faker.date.past({ years: 10 }),
-  }));
-
-  await db.insert(patients).values(patientsMock).returning();
-
-  console.log("Initial patients created.");
-}
-
 async function seed() {
-  await createInitialPatients();
   console.log(
     "Seed completed. Note: Users are created via Google OAuth sign-in."
   );
