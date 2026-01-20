bd138ab1e538aa34650ee26e537385191e3506a6
Gonçalo Cantante <goncalocantante@gmail.com>
2026-01-20 23:09:01 +0000

    patient profile pages

--- lib/db/queries/patient-queries.ts
+++ lib/db/queries/patient-queries.ts
@@ -1,7 +1,7 @@
 import { getUser } from "./queries";
 import { db } from "../drizzle";
 import { patients } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
 export async function getPatients() {
   const user = await getUser();
@@ -15,4 +15,18 @@ export async function getPatients() {
     .where(eq(patients.userId, user.id));
 
   return patientsRes;
+}
+
+export async function getPatientById(id: string) {
+  const user = await getUser();
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const [patient] = await db
+    .select()
+    .from(patients)
+    .where(and(eq(patients.id, id), eq(patients.userId, user.id)));
+
+  return patient;
 }
