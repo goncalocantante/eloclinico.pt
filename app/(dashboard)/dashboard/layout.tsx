57360739d2ec78ede3decd1d2340ce9a870921cc
Gonçalo Cantante <goncalocantante@gmail.com>
2026-02-13 15:49:41 +0000

    add SidebarTrigger for mobile devices

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -2,9 +2,14 @@ import { CalendarProvider } from "@/calendar/contexts/calendar-context";
 import { AddAppointmentDialog } from "@/calendar/components/dialogs/add-appointment-dialog";
 import { IUser } from "@/calendar/interfaces";
 import { AppSidebar } from "@/components/app-sidebar/app-sidebar";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { ConfirmProvider } from "@/contexts/confirm-action-context";
 import { BreadcrumbComponent } from "@/components/breadcrumb-component";
+
 const users: IUser[] = [
   { id: "mock-id", name: "Mock User", picturePath: null },
 ];
@@ -20,6 +25,9 @@ export default function DashboardLayout({
         <ConfirmProvider>
           <AppSidebar />
           <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 md:hidden">
+              <SidebarTrigger className="-ml-1" />
+            </header>
             <main className="flex-1 overflow-y-auto p-0 lg:p-4">
               <BreadcrumbComponent />
               {children}
